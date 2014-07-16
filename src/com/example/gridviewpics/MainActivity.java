6517d7ee6133890265c5ca8d876d3b64b324bf9b
zshaheen <zeshawns@sbcglobal.net>
2014-07-15 21:40:02 -0700

    Caching implemented, but still lagging
    Will try to do the loading off the UI Thread next.

--- src/com/example/gridviewpics/MainActivity.java
+++ src/com/example/gridviewpics/MainActivity.java
@@ -12,6 +12,7 @@ import android.media.ThumbnailUtils;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
+import android.util.LruCache;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -25,6 +26,10 @@ import android.os.Build;
 public class MainActivity extends Activity {
 	String[] fileNames;
 	File path;
+	private LruCache<String, Bitmap> mMemoryCache;
+
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +45,17 @@ public class MainActivity extends Activity {
             //	Log.e("paths",path.getPath()+"/"+ fileNames[i]);
         }
         
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+        final int cacheSize = maxMemory / 8;
+        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                // The cache size will be measured in kilobytes rather than
+                // number of items.
+                return bitmap.getByteCount() / 1024;
+            }
+        };
+
         
       //Create a gird view in activity_main.xml with id of 'gridview'
         GridView gridView = (GridView) findViewById(R.id.gridview);
@@ -62,17 +78,26 @@ public class MainActivity extends Activity {
         	//Do something, Toast an error, create a warning
         	Log.e("fileName[]", "Path DOESNT exist");
         }
-        */	
+        */
         
-        
-        
-        
+    }
+    
+    
+    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+        if (getBitmapFromMemCache(key) == null) {
+            mMemoryCache.put(key, bitmap);
+        }
+    }
+
+    public Bitmap getBitmapFromMemCache(String key) {
+        return mMemoryCache.get(key);
     }
     
     
     public class ImageAdapter extends BaseAdapter {
         private Context mContext;
         int size = (int) getResources().getDimension(R.dimen.image_size);
+        Bitmap bitmap = null;
 
         public ImageAdapter(Context c) {
             mContext = c;
@@ -90,7 +115,47 @@ public class MainActivity extends Activity {
         public long getItemId(int position) {
             return position;
         }
+        
+        public int calculateInSampleSize(
+                BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
 
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+        
+        public Bitmap decodeSampledBitmapFromFile(String filePath, int reqWidth, int reqHeight) {
+			
+        	final BitmapFactory.Options options = new BitmapFactory.Options();
+        	options.inJustDecodeBounds = true;
+        	BitmapFactory.decodeFile(filePath,options);
+        	
+            // Calculate inSampleSize
+            options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+        	//options.inSampleSize = 128;
+        	
+            // Decode bitmap with inSampleSize set
+            options.inJustDecodeBounds = false;
+            return BitmapFactory.decodeFile(filePath,options);
+	
+        	
+        }
+        
         // create a new ImageView for each item referenced by the Adapter
         public View getView(int position, View convertView, ViewGroup parent) {
             ImageView imageView;
@@ -102,28 +167,31 @@ public class MainActivity extends Activity {
             } else {
                 imageView = (ImageView) convertView;
             }
-            
-            //Load in the direcroty and display the images
-            /*String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/SnapDrawShare/";
-            File path = new File(dir);
-
-            if(path.isDirectory()) {
-            	//Log.e("File path", "Path is a directory");
-            	String[] fileNames = path.list();
-            	//Log.e("fileName.length", Integer.toString(fileNames.length));
-            	*/
-            	//for(int i=0; i<fileNames.length; i++) {
-            	//	Log.e("fileName[]", fileNames[i]);
-            	//}
+            /*
             BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inSampleSize = 13;
+            //options.inSampleSize = 16;
             //options.inJustDecodeBounds = true;
-            	Bitmap mBitmap = BitmapFactory.decodeFile(path.getPath()+"/"+ fileNames[position],options);
-            	imageView.setImageBitmap(ThumbnailUtils.extractThumbnail(mBitmap, size, size));
-           // }
+			Bitmap mBitmap = BitmapFactory.decodeFile(path.getPath()+"/"+ fileNames[position],options);
+			imageView.setImageBitmap(mBitmap);
+            */
             
             
-            //imageView.setImageResource(mThumbIds[position]);
+            //attemps to load a bitmap with key of the filename
+            bitmap = getBitmapFromMemCache(fileNames[position]);
+            if (bitmap != null) {
+            	//image was found in cache
+                imageView.setImageBitmap(bitmap);
+            } else {
+            	//load in the image and save to cache
+            	bitmap = decodeSampledBitmapFromFile(path.getPath()+"/"+ fileNames[position],size,size);
+            	addBitmapToMemoryCache(fileNames[position], bitmap );
+            	
+            	imageView.setImageBitmap(bitmap);
+            }
+            
+            
+			//imageView.setImageBitmap(decodeSampledBitmapFromFile(path.getPath()+"/"+ fileNames[position],size,size));
+
             return imageView;
         }
     }   
