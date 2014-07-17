0648596f06cf77492922ac927fbdacd37911be2c
zshaheen <zeshawns@sbcglobal.net>
2014-07-16 19:18:45 -0700

    Scrolling is smooth, but pics not loading correctly

--- src/com/example/gridviewpics/MainActivity.java
+++ src/com/example/gridviewpics/MainActivity.java
@@ -1,27 +1,25 @@
 package com.example.gridviewpics;
 
 import java.io.File;
+import java.lang.ref.WeakReference;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.ThumbnailUtils;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.util.LruCache;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
-import android.os.Build;
 
 public class MainActivity extends Activity {
 	String[] fileNames;
@@ -93,6 +91,45 @@ public class MainActivity extends Activity {
         return mMemoryCache.get(key);
     }
     
+    public Bitmap decodeSampledBitmapFromFile(String filePath, int reqWidth, int reqHeight) {
+		
+    	final BitmapFactory.Options options = new BitmapFactory.Options();
+    	options.inJustDecodeBounds = true;
+    	BitmapFactory.decodeFile(filePath,options);
+    	
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+    	//options.inSampleSize = 128;
+    	
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeFile(filePath,options);
+
+    	
+    }
+    
+    public int calculateInSampleSize(
+	            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+	    // Raw height and width of image
+	    final int height = options.outHeight;
+	    final int width = options.outWidth;
+	    int inSampleSize = 1;
+	
+	    if (height > reqHeight || width > reqWidth) {
+	
+	        final int halfHeight = height / 2;
+	        final int halfWidth = width / 2;
+	
+	        // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+	        // height and width larger than the requested height and width.
+	        while ((halfHeight / inSampleSize) > reqHeight
+	                && (halfWidth / inSampleSize) > reqWidth) {
+	            inSampleSize *= 2;
+	        }
+	    }
+	
+	    return inSampleSize;
+	}
     
     public class ImageAdapter extends BaseAdapter {
         private Context mContext;
@@ -116,45 +153,6 @@ public class MainActivity extends Activity {
             return position;
         }
         
-        public int calculateInSampleSize(
-                BitmapFactory.Options options, int reqWidth, int reqHeight) {
-        // Raw height and width of image
-        final int height = options.outHeight;
-        final int width = options.outWidth;
-        int inSampleSize = 1;
-
-        if (height > reqHeight || width > reqWidth) {
-
-            final int halfHeight = height / 2;
-            final int halfWidth = width / 2;
-
-            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
-            // height and width larger than the requested height and width.
-            while ((halfHeight / inSampleSize) > reqHeight
-                    && (halfWidth / inSampleSize) > reqWidth) {
-                inSampleSize *= 2;
-            }
-        }
-
-        return inSampleSize;
-    }
-        
-        public Bitmap decodeSampledBitmapFromFile(String filePath, int reqWidth, int reqHeight) {
-			
-        	final BitmapFactory.Options options = new BitmapFactory.Options();
-        	options.inJustDecodeBounds = true;
-        	BitmapFactory.decodeFile(filePath,options);
-        	
-            // Calculate inSampleSize
-            options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
-        	//options.inSampleSize = 128;
-        	
-            // Decode bitmap with inSampleSize set
-            options.inJustDecodeBounds = false;
-            return BitmapFactory.decodeFile(filePath,options);
-	
-        	
-        }
         
         // create a new ImageView for each item referenced by the Adapter
         public View getView(int position, View convertView, ViewGroup parent) {
@@ -174,10 +172,11 @@ public class MainActivity extends Activity {
 			Bitmap mBitmap = BitmapFactory.decodeFile(path.getPath()+"/"+ fileNames[position],options);
 			imageView.setImageBitmap(mBitmap);
             */
+
             
             
             //attemps to load a bitmap with key of the filename
-            bitmap = getBitmapFromMemCache(fileNames[position]);
+            /*bitmap = getBitmapFromMemCache(fileNames[position]);
             if (bitmap != null) {
             	//image was found in cache
                 imageView.setImageBitmap(bitmap);
@@ -187,14 +186,125 @@ public class MainActivity extends Activity {
             	addBitmapToMemoryCache(fileNames[position], bitmap );
             	
             	imageView.setImageBitmap(bitmap);
-            }
+            }*/
             
-            
-			//imageView.setImageBitmap(decodeSampledBitmapFromFile(path.getPath()+"/"+ fileNames[position],size,size));
+            //imageView.setImageBitmap();
+            loadBitmap(path.getPath()+"/"+ fileNames[position], imageView );
+            //imageView.setImageBitmap(decodeSampledBitmapFromFile(path.getPath()+"/"+ fileNames[position],size,size));
 
             return imageView;
         }
     }   
+    
+    
+    public static boolean cancelPotentialWork(String fileName, ImageView imageView) {
+        final BitmapWorkerTask bitmapWorkerTask = getBitmapWorkerTask(imageView);
+        
+        if (bitmapWorkerTask != null) {
+            final String bitmapString = bitmapWorkerTask.filename;
+            // If bitmapData is not yet set or it differs from the new data
+            if ( bitmapString != fileName) {
+                // Cancel previous task
+                bitmapWorkerTask.cancel(true);
+            } else {
+                // The same work is already in progress
+                return false;
+            }
+        }
+        // No task associated with the ImageView, or an existing task was cancelled
+        return true;
+    }
+    
+    private static BitmapWorkerTask getBitmapWorkerTask(ImageView imageView) {
+    	   if (imageView != null) {
+    		   
+    		   //Drawable is null!!!!
+    	       final Drawable drawable = imageView.getDrawable();
+    	       
+    	       //if(drawable == null)
+    	    	  // Log.e("error", "drwawable is null");
+    	       
+    	       if (drawable instanceof AsyncDrawable) {
+    	    	   Log.e("Shit","Drawable is of type AsyncDrawable");
+    	           final AsyncDrawable asyncDrawable = (AsyncDrawable) drawable;
+    	           return asyncDrawable.getBitmapWorkerTask();
+    	       }
+    	    }
+    	   Log.e("error", "getBitmapWorkerTask returns null");
+    	    return null;
+    	}
+    
+    public void loadBitmap(String filename, ImageView imageView) {
+    	/////////////////////////////////if (cancelPotentialWork(filename, imageView)) {
+            final BitmapWorkerTask task = new BitmapWorkerTask(imageView);
+            //Dont need code below because I'm not placing the temp image in the imageview.
+            //The temp image is the default image shown before something is loaded.
+            //final AsyncDrawable asyncDrawable =
+                    //new AsyncDrawable(getResources(), mPlaceHolderBitmap, task);
+            //imageView.setImageDrawable(asyncDrawable);
+            task.execute(filename);
+/////////////////////////////////}
+    }
+    
+    public class BitmapWorkerTask extends AsyncTask<String, Void, Bitmap> {
+        private final WeakReference<ImageView> imageViewReference;
+        private String filename;
+        int size = (int) getResources().getDimension(R.dimen.image_size);
+        
+        public BitmapWorkerTask(ImageView imageView) {
+            // Use a WeakReference to ensure the ImageView can be garbage collected
+            imageViewReference = new WeakReference<ImageView>(imageView);
+        }
+
+        // Decode image in background.
+        @Override
+        protected Bitmap doInBackground(String... params) {
+        	filename = params[0];
+            return decodeSampledBitmapFromFile(filename, size, size);
+        }
+
+        // Once complete, see if ImageView is still around and set bitmap.
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+        	if (isCancelled()) {
+                bitmap = null;
+            }
+
+            if (imageViewReference != null && bitmap != null) {
+                final ImageView imageView = imageViewReference.get();
+                //This is not being loaded correctly
+                final BitmapWorkerTask bitmapWorkerTask =
+                        getBitmapWorkerTask(imageView);
+                
+                //Log.e("bitmapWorkerTask", bitmapWorkerTask.filename);
+                //Log.e("bitmapWorkerTask", Integer.toString(bitmapWorkerTask.size));
+                
+                	//Log.e("Stuff", "this == bitmapWorkerTask");
+                //Log.e("Stuff", "this == bitmapWorkerTask");
+                if (/*this == bitmapWorkerTask &&*/ imageView != null) {
+                    imageView.setImageBitmap(bitmap);
+                }
+            }
+            
+        }
+    }
+    
+    
+    static class AsyncDrawable extends BitmapDrawable {
+        private final WeakReference<BitmapWorkerTask> bitmapWorkerTaskReference;
+        
+        //Something is wrong below
+        public AsyncDrawable(Resources res, Bitmap bitmap,
+                BitmapWorkerTask bitmapWorkerTask) {
+            super(res, bitmap);
+            bitmapWorkerTaskReference =
+                new WeakReference<BitmapWorkerTask>(bitmapWorkerTask);
+        }
+
+        public BitmapWorkerTask getBitmapWorkerTask() {
+            return bitmapWorkerTaskReference.get();
+        }
+    }
     
 }
 
